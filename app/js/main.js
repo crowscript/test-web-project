3d54d688611d65fa30027132d953b9df2c8bbfc0
Your Name <you@example.com>
2017-12-14 23:58:25 +0100

    project done

--- app/js/main.js
+++ app/js/main.js
@@ -1,49 +1,162 @@
-/*
-Reference: http://jsfiddle.net/BB3JK/47/
-*/
 
-$('select').each(function(){
-    var $this = $(this), numberOfOptions = $(this).children('option').length;
-  
-    $this.addClass('select-hidden'); 
-    $this.wrap('<div class="select"></div>');
-    $this.after('<div class="select-styled"></div>');
 
-    var $styledSelect = $this.next('div.select-styled');
-    $styledSelect.text($this.children('option').eq(0).text());
-  
-    var $list = $('<ul />', {
-        'class': 'select-options'
-    }).insertAfter($styledSelect);
-  
-    for (var i = 0; i < numberOfOptions; i++) {
-        $('<li />', {
-            text: $this.children('option').eq(i).text(),
-            rel: $this.children('option').eq(i).val()
-        }).appendTo($list);
-    }
-  
-    var $listItems = $list.children('li');
-  
-    $styledSelect.click(function(e) {
-        e.stopPropagation();
-        $('div.select-styled.active').not(this).each(function(){
-            $(this).removeClass('active').next('ul.select-options').hide();
-        });
-        $(this).toggleClass('active').next('ul.select-options').toggle();
-    });
-  
-    $listItems.click(function(e) {
-        e.stopPropagation();
-        $styledSelect.text($(this).text()).removeClass('active');
-        $this.val($(this).attr('rel'));
-        $list.hide();
-        //console.log($this.val());
-    });
-  
-    $(document).click(function() {
-        $styledSelect.removeClass('active');
-        $list.hide();
-    });
+  // With JQuery
+	$('#ex1').slider({
+		formatter: function(value) {
+			return  value + ' km/h';
+		}
+	});
 
-});
+
+
+
+
+	$(function(){
+		$("#submit").on("click", function () {
+			$(this).addClass('animated bounceIn').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+				$(this).removeClass('animated bounceIn');
+			});
+		});
+	});
+	
+	$(function(){
+		$(".select-styled").on("click", function () {
+			$('.select').addClass('animated bounceIn').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+				$('.select').removeClass('animated bounceIn');
+			});
+		});
+	});
+
+	$(function(){
+		$("input").on("click", function () {
+			$(this).next('label').addClass('animated bounceIn').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+				$("input").next('label').removeClass('animated bounceIn');
+			});
+		});
+	});
+
+	$(function(){
+		$("#submit").on("click", function () {
+			$(".finale--car").addClass('animated animatedx fadeInRight').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+				$(".finale--car").removeClass('animated animatedx fadeInRight');
+			});
+		});
+	});
+
+	$(document).ready(function () {
+			$("#play").unbind().on("click", function () {
+				$(".page--second").addClass("hidden");
+				$(".page--first").removeClass("hidden");
+				$(".finale--img").addClass("hidden");
+			});
+	});
+
+
+	
+	 
+//
+//$("select" ).change(function() {
+//    var str = "";
+//    $( "select option:selected" ).each(function() {
+//      str += $( this ).text() + " ";
+//    });
+//    $( "#op" ).text( str );
+//  })
+//  .trigger( "change" );
+//
+//$(document).on("change","select",function(){
+//  $("option[value=" + this.value + "]", this)
+//  .attr("selected", true).siblings()
+//  .removeAttr("selected")
+//});
+
+
+
+// car showing
+
+	$(document).ready(function () {
+			$("#submit").unbind().on("click", function () {
+					if ($("#radio1").is(':checked') && $("#radio4").is(':checked')) {
+							$("#car1gray").removeClass("hidden");
+							$("#img2").addClass("hidden");
+					}
+					else if ($("#radio1").is(':checked') && $("#radio5").is(':checked')) {
+							$("#car1green").removeClass("hidden");
+					}
+					else if ($("#radio1").is(':checked') && $("#radio6").is(':checked')) {
+							$("#car1turquoise").removeClass("hidden");
+					}
+					else if ($("#radio2").is(':checked') && $("#radio4").is(':checked')) {
+							$("#car2gray").removeClass("hidden");
+					}
+					else if ($("#radio2").is(':checked') && $("#radio5").is(':checked')) {
+							$("#car2green").removeClass("hidden");
+					}
+					else if ($("#radio2").is(':checked') && $("#radio6").is(':checked')) {
+							$("#car2turquoise").removeClass("hidden");
+					}
+					else if ($("#radio3").is(':checked') && $("#radio4").is(':checked')) {
+							$("#car3gray").removeClass("hidden");
+					}
+					else if ($("#radio3").is(':checked') && $("#radio5").is(':checked')) {
+							$("#car3green").removeClass("hidden");
+					}
+					else if ($("#radio3").is(':checked') && $("#radio6").is(':checked')) {
+							$("#car3turquoise").removeClass("hidden");
+					}
+
+					if ($("#radio7").is(':checked')) {
+							$("#tire1").removeClass("hidden");
+					}
+					else if ($("#radio8").is(':checked')) {
+							$("#tire2").removeClass("hidden");
+					}
+					else if ($("#radio9").is(':checked')) {
+							$("#tire3").removeClass("hidden");
+					}
+
+					if ($("#radio10").is(':checked')) {
+							$("#window1").removeClass("hidden");
+					}
+					else if ($("#radio11").is(':checked')) {
+							$("#window2").removeClass("hidden");
+					}
+					else if ($("#radio12").is(':checked')) {
+							$("#window3").removeClass("hidden");
+					}
+
+				$(".page--second").removeClass("hidden");
+				$(".page--first").addClass("hidden");
+				var shapeValue =  parseInt($('input[name=shape]:checked').val());
+				var colorValue =  parseInt($('input[name=color]:checked').val());
+				var tireValue =  parseInt($('input[name=tire]:checked').val());
+				var windowValue =  parseInt($('input[name=window]:checked').val());
+				var sliderValue =  $('#ex1').attr('aria-valuenow', sliderValue).val();
+				var dropdownValue = parseInt($('select').attr("selected", true).val());
+			
+				
+				var finaleResult = 2000 + shapeValue + colorValue + tireValue + windowValue + parseInt(sliderValue) + dropdownValue;
+
+
+				$("#result").text(finaleResult);
+
+				
+				$(this).tooltip('show');
+				
+			});
+		});
+
+	$(document).ready(function () {
+		$("#submit").click(function () {
+			$('html,body').animate({
+					scrollTop: $(".page--first").offset().top + 420
+			},
+					'slow');
+		});
+	});
+
+	$(document).on("change","select",function(){
+		$("option[value=" + this.value + "]", this)
+		.attr("selected", true).siblings()
+		.removeAttr("selected");
+	});
